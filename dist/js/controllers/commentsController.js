e6d07cd4c97688d533c9c13f4071bd771766a4a2
Calvin Crane <calvincrane@gmail.com>
2015-06-10 18:06:39 +0100

    removing directories from repo that are libs and published

--- dist/js/controllers/commentsController.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var commentsController = angular.module('commentsController', []);
-modalController.controller('CommentsCtrl',['$scope','$sce','modalService','myGetService','$compile', function($scope,$sce,modalService,myGetService,$compile) {
-	console.log('commetns control');
-
-}]);
